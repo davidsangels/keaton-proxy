1e858557118c44c34c57fe54833b1d5b83b3f628
Keaton Tatooles <keatontatooles@Keatons-MacBook-Air.local>
2019-07-15 16:47:20 -0700

    wrote http-proxy to redirect get request for booking to data to booking service server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
+const reservationServer = 'http://localhost:3001';
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(morgan('dev'));
+
+app.get('/bookings', (req, res) => {
+    apiProxy.web(req, res, {target: reservationServer});
+});
 
 app.listen(3000);
